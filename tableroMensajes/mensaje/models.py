6563561db621dd1e3c7b91c5a29a9afae0b83faa
Marce Delgado <madelgado1@udc.edu.ar>
2024-08-26 19:09:27 -0300

    Implementacion del modelo

--- tableroMensajes/mensaje/models.py
+++ tableroMensajes/mensaje/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Mensaje (models.Model):
+    textoDelMensaje = models.TextField()
+    remitente = models.CharField(max_length = 200)
+    destinatario = models.CharField(max_length = 200)
+    fechayHora = models.DateTimeField(default = timezone.now)
+    
 
